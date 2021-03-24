ccf1e2872334ce2df0366a55a2ebdbfa52a86ec1
VandittSama <sama.vanditt@gmail.com>
2021-03-23 22:55:11 -0700

    Added project owner logic to backend

--- backend/flask_service/main_service.py
+++ backend/flask_service/main_service.py
@@ -5,7 +5,6 @@ import hashlib
 import datetime
 from flask import jsonify, json
 
-
 app = Flask(__name__)
 
 app.config['MYSQL_USER'] = 'root'
@@ -47,19 +46,16 @@ def login():
     print("i am in loginnnnn")
     user_email = request.args.get('email')
     cur = mysql.connection.cursor()
-    
-     # Checking if the user exists
+
+    # Checking if the user exists
     userExists = cur.execute("SELECT name FROM user WHERE email_id=%s",
-                              (user_email, ))
+                             (user_email, ))
     if (userExists == 0):
         return "User Doesn't Exist", 401
 
     else:
         token = jwt.encode({'email': user_email}, 'secret', algorithm='HS256')
         return jsonify({'token': token}), 201
-        #  <add code to tokenify email>
-        
-
 
 
 @app.route("/newProject", methods=['POST'])
@@ -82,8 +78,8 @@ def newProject():
         "SELECT project_name FROM projects WHERE project_id=%s", (proj_id, ))
     if (projExists == 0):
         # Adding a new project to the project table
-        cur.execute("INSERT INTO projects VALUES (%s,%s,%s)",
-                    (proj_name, proj_id, proj_desc))
+        cur.execute("INSERT INTO projects VALUES (%s,%s,%s,%s)",
+                    (proj_name, proj_id, proj_desc, user_email))
 
         # Allocating project to the current user
         cur.execute("INSERT INTO user_projects VALUES (%s,%s)",
@@ -98,7 +94,6 @@ def newProject():
 
 @app.route("/projects")
 def listProjects():
-    reqData = request.json
     user_email = request.args.get('email')
     cur = mysql.connection.cursor()
 
@@ -113,21 +108,21 @@ def listProjects():
     for project in cur.fetchall():
         project_IDs.append(project['project_id'])
 
-    sql = 'SELECT project_name, project_id, project_desc FROM projects WHERE project_id IN (%s)'
+    sql = 'SELECT project_name, project_id, project_desc, owner_email FROM projects WHERE project_id IN (%s)'
     in_p = ', '.join(list(map(lambda x: '%s', project_IDs)))
     sql = sql % in_p
     cur.execute(sql, project_IDs)
     project_list = []
-    
+
     for row in cur.fetchall():
         temp = {}
         temp["project_id"] = row['project_id']
         temp["project_name"] = row['project_name']
         temp["project_desc"] = row['project_desc']
+        temp["owner_email"] = row['owner_email']
         project_list.append(temp)
     print("project list :" + str(project_list))
     return json.dumps(project_list), 200
-
 
 
 @app.route("/addProjectUser", methods=['POST'])
@@ -173,8 +168,7 @@ def addProjectUser():
 
 @app.route("/projectUsers")
 def projectUsers():
-    reqData = request.json
-    project_id = reqData['project_id']
+    project_id = request.args.get('project_id')
     cur = mysql.connection.cursor()
 
     num_users = cur.execute(
@@ -202,8 +196,7 @@ def projectUsers():
 
 @app.route("/project", methods=['GET', 'DELETE'])
 def getOrDeleteProject():
-    reqData = request.json
-    project_id = reqData['project_id']
+    project_id = request.args.get('project_id')
     cur = mysql.connection.cursor()
     projExists = cur.execute("SELECT * FROM projects WHERE project_id=%s",
                              (project_id, ))
@@ -216,6 +209,7 @@ def getOrDeleteProject():
             project_details['project_name'] = row['project_name']
             project_details['project_id'] = row['project_id']
             project_details['project_desc'] = row['project_desc']
+            project_details['owner_email'] = row['owner_email']
         return json.dumps(project_details), 201
 
     elif request.method == 'DELETE':
