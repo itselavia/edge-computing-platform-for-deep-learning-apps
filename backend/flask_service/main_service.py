687230766974de4775861c7e88348b1156f84932
VandittSama <sama.vanditt@gmail.com>
2021-03-24 02:22:41 -0700

    CORS changes

--- backend/flask_service/main_service.py
+++ backend/flask_service/main_service.py
@@ -23,6 +23,18 @@ def index():
     return "Home"
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin',
+                         'http://localhost:3001')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+
+
 @app.route("/newUser", methods=['POST'])
 def newUser():
     reqData = request.json
