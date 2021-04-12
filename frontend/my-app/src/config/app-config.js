da8b4300e998073b8fb97d073bd37d1778d538d8
AISHWARYA DEEP RASTOGI <aishwaryaicerastogi@gmail.com>
2021-04-12 13:14:57 -0700

    port changed

--- frontend/my-app/src/config/app-config.js
+++ frontend/my-app/src/config/app-config.js
@@ -1,6 +1,6 @@
 const DOMAIN = process.env.REACT_APP_DOMAIN;
 const config = {
-    api_host: "http://"+process.env.REACT_APP_DOMAIN+":34000/",
+    api_host: "http://"+process.env.REACT_APP_DOMAIN+":31000/",
     pods_info_base: "http://"+process.env.REACT_APP_DOMAIN+":32000/"
 }
 console.log("Hi i am in config")
